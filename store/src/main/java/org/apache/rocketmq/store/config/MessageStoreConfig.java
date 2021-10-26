d7e6017b31bd6603a7f9eeac4da02e6ab8950795
kingkong <kongq1983@qq.com>
2021-10-26 21:21:28 +0800

    加排序

--- store/src/main/java/org/apache/rocketmq/store/config/MessageStoreConfig.java
+++ store/src/main/java/org/apache/rocketmq/store/config/MessageStoreConfig.java
@@ -32,7 +32,7 @@ public class MessageStoreConfig {
 
     // CommitLog file size,default is 1G
     private int mappedFileSizeCommitLog = 1024 * 1024 * 1024;
-    // ConsumeQueue file size,default is 30W
+    // ConsumeQueue file size,default is 30W * 20（每条20字节）
     private int mappedFileSizeConsumeQueue = 300000 * ConsumeQueue.CQ_STORE_UNIT_SIZE;
     // enable consume queue ext
     private boolean enableConsumeQueueExt = false;
